c1e6d85d77993cd5730e8e4ae21df93ec30af412
Leandro Klyte <klyte45@hotmail.com>
2023-02-26 15:28:02 -0300

    Added basic files for the main project

---
+++ CustomZoneMixer/CZMController.cs
@@ -0,0 +1,13 @@
+﻿using Kwytto.Interfaces;
+
+namespace Klyte.CustomZoneMixer
+{
+    public class CZMController : BaseController<ModInstance, CZMController>
+    {
+        protected void Awake()
+        {
+
+        }
+
+    }
+}
---
+++ CustomZoneMixer/ModInstance.cs
@@ -0,0 +1,26 @@
+﻿using CustomZoneMixer.Localization;
+using Kwytto.Interfaces;
+using System.Globalization;
+using System.Reflection;
+using UnityEngine;
+
+[assembly: AssemblyVersion("0.0.0.*")]
+namespace Klyte.CustomZoneMixer
+{
+    public class ModInstance : BasicIUserMod<ModInstance, CZMController>
+    {
+        public override string SimpleName => "Custom Zone Mixer";
+
+        public override string SafeName => "CustomZoneMixer";
+
+        public override string Acronym => "CZM";
+
+        public override Color ModColor => new Color32(0x66, 0x26, 0x24, 0xFF);
+
+        public override string Description => Str.root_modDescription;
+        protected override void SetLocaleCulture(CultureInfo culture)
+        {
+            Str.Culture = culture;
+        }
+    }
+}
